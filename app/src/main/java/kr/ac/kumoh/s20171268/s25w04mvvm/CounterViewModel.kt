8796a0d9f6d2c26760dc6959638c34d09ce1201c
choongmoo <wdokl88@naver.com>
2025-10-01 14:29:50 +0900

    🚀SharedFlow 사용

--- app/src/main/java/kr/ac/kumoh/s20171268/s25w04mvvm/CounterViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20171268/s25w04mvvm/CounterViewModel.kt
@@ -1,22 +1,26 @@
 package kr.ac.kumoh.s20171268.s25w04mvvm
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class CounterViewModel : ViewModel() {
-    //private val _counterState = mutableStateOf(CounterModel(0))
     private val _counterState = MutableStateFlow(CounterModel(0))
-
-    //val counterState: State<CounterModel> = _counterState
     val counterState: StateFlow<CounterModel> = _counterState
+
+    // 변수 이름: _events
+    private val _events = MutableSharedFlow<String>()
+    val events = _events.asSharedFlow()
 
     fun addCounter() {
         viewModelScope.launch {
-            _counterState.update { currentModel ->
-                CounterModel(count = currentModel.count + 1)
+            _counterState.value = CounterModel(_counterState.value.count + 1)
+            if (_counterState.value.count == 5) {
+                _events.emit("카운터 5에 도달")
             }
         }
     }
